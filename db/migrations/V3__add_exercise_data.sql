5083e5bfb80193ab8a53b19ea52bad2ace7ccaf9
Daniel Moffat <danmofo@gmail.com>
2024-07-09 20:16:20 +0100

    Added exercise migration

---
+++ db/migrations/V3__add_exercise_data.sql
@@ -0,0 +1,3 @@
+insert into ft.exercise (`name`, brand, `type`) values ('Back squat', null, 'FREE_WEIGHT');
+insert into ft.exercise (`name`, brand, `type`) values ('Barbell Bench press', null, 'FREE_WEIGHT');
+insert into ft.exercise (`name`, brand, `type`) values ('Deadlift', null, 'FREE_WEIGHT');
