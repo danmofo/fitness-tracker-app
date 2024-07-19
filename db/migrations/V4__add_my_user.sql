38c18316c9615ea8fbbbbcb02caad2522472de95
Daniel Moffat <danmofo@gmail.com>
2024-07-19 18:07:12 +0100

    Add migration which creates a test user (for consistent tests) + add historical weight data for testing app view

--- db/migrations/V4__add_my_user.sql
+++ db/migrations/V4__add_my_user.sql
@@ -1,1 +1,5 @@
+-- Create a test user
+insert into ft.user (email, `password`, created_on) values ('danmofo+ft-test@gmail.com', '{bcrypt}$2a$10$Kyi7f2xlP8Js9RSf3h0XpOgip6QUiMjLsScgRfe1JCIZooShwvA/m', now());
+
+-- Create my user
 insert into ft.user (email, `password`, created_on) values ('danmofo@gmail.com', '{bcrypt}$2a$10$Kyi7f2xlP8Js9RSf3h0XpOgip6QUiMjLsScgRfe1JCIZooShwvA/m', now());
