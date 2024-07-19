829b3fdd68e9dd9534b158535d7a7b1b9fbdd201
Daniel Moffat <danmofo@gmail.com>
2024-07-19 18:52:26 +0100

    Add the ability to log out

--- mobile-app/app/dashboard/index.tsx
+++ mobile-app/app/dashboard/index.tsx
@@ -2,20 +2,32 @@ import Button from "@/components/Button";
 import Box from "@/components/layout/Box";
 import ScreenLayout from "@/components/layout/ScreenLayout";
 import Heading from "@/components/text/Heading";
+import { useAuthStore } from "@/store/auth-store";
+import { router } from "expo-router";
 import { Text } from "react-native";
 
 export default function DashboardScreen() {
+    const auth = useAuthStore();
+
+    function handleLogOut() {
+        auth.logOut();
+        router.navigate('/');
+    }
+
     return (
         <ScreenLayout screenHasHeader={false}>
-            <Box flex={1} padding={20}>
+            <Box padding={20}>
                 <Heading>Log workout</Heading>
                 <Text>Here you can log your workout</Text>
                 <Button title="Start workout" href="start-workout" />
             </Box>
-            <Box flex={1} padding={20}>
+            <Box padding={20}>
                 <Heading>Log weight</Heading>
                 <Text>Here you can log your weight</Text>
                 <Button title="Log weight" href="/log-weight/" />
+            </Box>
+            <Box padding={20} paddingVertical={40}>
+                <Button title="Log out" onPress={handleLogOut} />
             </Box>
         </ScreenLayout>
     )
